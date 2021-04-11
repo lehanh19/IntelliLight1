f5d547126ea9002d4bb4f3590f7294825ecccbea
lehanh19 <52921841+lehanh19@users.noreply.github.com>
2021-04-11 12:34:45 +0700

    Update map_computor.py

--- map_computor.py
+++ map_computor.py
@@ -92,14 +92,9 @@ direction_lane_dict = {"NSG": [1, 0], "SNG": [1, 0], "EWG": [1, 0], "WEG": [1, 0
                        "NEG": [2], "WNG": [2], "SWG": [2], "ESG": [2]}
 direction_list = ["NWG", "WSG", "SEG", "ENG", "NSG", "SNG", "EWG", "WEG", "NEG", "WNG", "SWG", "ESG"]
 
-#min_phase_time = [30, 96, 74]
-# min_phase_time_7 = [10, 35]
 node_light_7 = "node0"
-# phases_light_7 = ["WNG_ESG_EWG_WEG_WSG_ENG", "NSG_NEG_SNG_SWG_NWG_SEG"]
-# WNG_ESG_EWG_WEG_WSG_ENG = "grrr gGGG grrr gGGG".replace(" ", "")
-# NSG_NEG_SNG_SWG_NWG_SEG = "gGGG grrr gGGG grrr".replace(" ", "")
-# controlSignal = (WNG_ESG_EWG_WEG_WSG_ENG, NSG_NEG_SNG_SWG_NWG_SEG)
-min_phase_time_7 = [9, 35, 9, 35]
+
+min_phase_time_7 = [35, 35, 35, 35]
 phases_light_7 = ["WNG_ESG", "WEG_EWG", "SWG_NEG", "SNG_NSG"]
 WNG_ESG = "grrG grrr grrG grrr".replace(" ", "")
 WEG_EWG = "gGGr grrr gGGr grrr".replace(" ", "")
@@ -107,9 +102,10 @@ SWG_NEG = "grrr grrG grrr grrG".replace(" ", "")
 SNG_NSG = "grrr gGGr grrr gGGr".replace(" ", "")
 controlSignal = (WNG_ESG, WEG_EWG, SWG_NEG, SNG_NSG)
 
-
-listLanes=['edge1-0_0','edge1-0_1','edge1-0_2','edge2-0_0','edge2-0_1','edge2-0_2',
-                                 'edge3-0_0','edge3-0_1','edge3-0_2','edge4-0_0','edge4-0_1','edge4-0_2']
+listLanes = ['edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2',
+            'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2']
+# listLanes=['edge1-0_0','edge1-0_1','edge1-0_2','edge2-0_0','edge2-0_1','edge2-0_2',
+#                                  'edge3-0_0','edge3-0_1','edge3-0_2','edge4-0_0','edge4-0_1','edge4-0_2']
 '''
 input: phase "NSG_SNG" , four lane number, in the key of W,E,S,N
 output:
@@ -178,24 +174,6 @@ def coordinate_mapper(x1, y1, x2, y2, area_length=600, area_width=600):
     width_num_grids = int(area_width / grid_width)
     return length_num_grids - y_max, length_num_grids - y_min, x_min, x_max
 
-def get_phase_affected_lane_traffic_max_volume(phase="NSG_SNG", tl_node_id="node0",
-                                 WESN_node_ids={"W": "1", "E": "2", "S": "3", "N": "4"}):
-    four_lane_ids_dict = find_surrounding_lane_WESN(central_node_id=tl_node_id, WESN_node_ids=WESN_node_ids)
-    directions = phase.split('_')
-    traffic_volume_start_end = []
-    for direction in directions:
-        traffic_volume_start_end.append([four_lane_ids_dict[direction[0]],four_lane_ids_dict[direction[1]]])
-    tree = ET.parse('./data/cross.rou.xml')
-    root = tree.getroot()
-    phase_volumes = []
-    for lane_id in traffic_volume_start_end:
-        to_lane_id="edge%s-%s"%(lane_id[1].split('-')[1],lane_id[1].split('-')[0][4:])
-        time_begin = root.find("./flow[@from='%s'][@to='%s']" % (lane_id[0], to_lane_id)).get('begin')
-        time_end = root.find("./flow[@from='%s'][@to='%s']" % (lane_id[0], to_lane_id)).get('end')
-        volume = root.find("./flow[@from='%s'][@to='%s']" % (lane_id[0], to_lane_id)).get('number')
-        phase_volumes.append((float(time_end)-float(time_begin))/float(volume))
-    return max(phase_volumes)
-
 
 def phase_affected_lane_position(phase="NSG_SNG", tl_node_id="node0",
                                  WESN_node_ids={"W": "1", "E": "2", "S": "3", "N": "4"}):
@@ -238,23 +216,12 @@ def vehicle_location_mapper(coordinate, area_length=600, area_width=600):
     return tempTransformTuple
 
 
-def translateAction(action):
-    result = 0
-    for i in range(len(action)):
-        result += (i + 1) * action[i]
-    return result
-
-
-
-
 def changeTrafficLight_7(current_phase=0):  # [WNG_ESG_WSG_ENG_NWG_SEG]
     # phases=["WNG_ESG_WSG_ENG_NWG_SEG","EWG_WEG_WSG_ENG_NWG_SEG","NSG_NEG_SNG_SWG_WSG_ENG_NWG_SEG"]
     next_phase = (current_phase + 1) % len(controlSignal)
     next_phase_time_eclipsed = 0
     traci.trafficlight.setRedYellowGreenState(node_light_7, controlSignal[next_phase])
     return next_phase, next_phase_time_eclipsed
-
-
 
 
 def get_phase_vector(current_phase=0):
@@ -264,31 +231,6 @@ def get_phase_vector(current_phase=0):
         phase_vector[direction_list.index(direction)] = 1
     return np.array(phase_vector)
 
-
-def getMapOfCertainTrafficLight(curtent_phase=0, tl_node_id="node0", area_length=600):
-    current_phases_light_7 = [phases_light_7[curtent_phase]]
-    parameterArray = phases_affected_lane_postions(phases=current_phases_light_7)
-    length_num_grids = int(area_length / grid_width)
-    resultTrained = np.zeros((length_num_grids, length_num_grids))
-    for affected_road in parameterArray:
-        resultTrained[affected_road[0]:affected_road[1], affected_road[2]:affected_road[3]] = 127
-    return resultTrained
-
-
-
-
-def calculate_reward(tempLastVehicleStateList):
-    waitedTime = 0
-    stop_count = 0
-    for key, vehicle_dict in tempLastVehicleStateList.items():
-        if tempLastVehicleStateList[key]['speed'] < 5:
-            waitedTime += 1
-            #waitedTime += (1 +math.pow(tempLastVehicleStateList[key]['waitedTime']/50,2))
-        if tempLastVehicleStateList[key]['former_speed'] > 0.5 and tempLastVehicleStateList[key]['speed'] < 0.5:
-            stop_count += (tempLastVehicleStateList[key]['stop_count']-tempLastVehicleStateList[key]['former_stop_count'])
-    #PI = (waitedTime + 10 * stop_count) / len(tempLastVehicleStateList) if len(tempLastVehicleStateList)!=0 else 0
-    PI = waitedTime/len(tempLastVehicleStateList) if len(tempLastVehicleStateList)!=0 else 0
-    return - PI
 
 def getMapOfVehicles(area_length=600):
     '''
@@ -336,8 +278,10 @@ def log_rewards(vehicle_dict, action, rewards_info_dict, file_name, timestamp,re
 
 
 def get_rewards_from_sumo(vehicle_dict, action, rewards_info_dict,
-                          listLanes=['edge1-0_0','edge1-0_1','edge1-0_2','edge2-0_0','edge2-0_1','edge2-0_2',
-                                 'edge3-0_0','edge3-0_1','edge3-0_2','edge4-0_0','edge4-0_1','edge4-0_2'],):
+                        #   listLanes=['edge1-0_0', 'edge1-0_1','edge1-0_2','edge2-0_0','edge2-0_1','edge2-0_2',
+                        #          'edge3-0_0','edge3-0_1','edge3-0_2','edge4-0_0','edge4-0_1','edge4-0_2'],
+                        listLanes = ['edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2',
+                                    'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2'],):
     reward = 0
     import copy
     reward_detail_dict = copy.deepcopy(rewards_info_dict)
@@ -366,16 +310,13 @@ def get_rewards_from_sumo(vehicle_dict, action, rewards_info_dict,
 
 
     for k, v in reward_detail_dict.items():
-        print("v", v)
         if v[0]:  # True or False
             reward += v[1]*v[2]
-    reward = restrict_reward(reward)#,func="linear")
-    # print("reward:", reward)
+    reward = restrict_reward(reward)
     return reward, reward_detail_dict
 
 def get_rewards_from_dict_list(rewards_detail_dict_list):
     reward = 0
-    # print("Ahihih", rewards_detail_dict_list)
     for i in range(len(rewards_detail_dict_list)):
         for k, v in rewards_detail_dict_list[i].items():
             if v[0]:  # True or False
@@ -494,9 +435,12 @@ def status_calculator():
     return [laneQueueTracker, laneNumVehiclesTracker, laneWaitingTracker, mapOfCars]
 
 def get_vehicle_id_entering():
+    # Đếm số lượng xe đi vào ngã 4. Khi xe gần đến ngã 4 thì bắt đầu đếm 
     vehicle_id_entering = []
-    entering_lanes = ['edge1-0_0', 'edge1-0_1', 'edge1-0_2', 'edge2-0_0', 'edge2-0_1', 'edge2-0_2',
-                     'edge3-0_0', 'edge3-0_1', 'edge3-0_2', 'edge4-0_0', 'edge4-0_1', 'edge4-0_2']
+    entering_lanes = ['edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2',
+                'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2', 'edge1-0_2']
+    # entering_lanes = ['edge1-0_0', 'edge1-0_1', 'edge1-0_2', 'edge2-0_0', 'edge2-0_1', 'edge2-0_2',
+    #                  'edge3-0_0', 'edge3-0_1', 'edge3-0_2', 'edge4-0_0', 'edge4-0_1', 'edge4-0_2']
 
     for lane in entering_lanes:
         vehicle_id_entering.extend(traci.lane.getLastStepVehicleIDs(lane))
@@ -513,38 +457,6 @@ def get_vehicle_id_leaving(vehicle_dict):
     return vehicle_id_leaving
 
 
-
-def get_car_on_red_and_green(cur_phase):
-    listLanes = ['edge1-0_0', 'edge1-0_1', 'edge1-0_2', 'edge2-0_0', 'edge2-0_1', 'edge2-0_2',
-                 'edge3-0_0', 'edge3-0_1', 'edge3-0_2', 'edge4-0_0', 'edge4-0_1', 'edge4-0_2']
-    vehicle_red = []
-    vehicle_green = []
-    if cur_phase == 1:
-        red_lanes = ['edge1-0_0', 'edge1-0_1', 'edge1-0_2', 'edge2-0_0', 'edge2-0_1', 'edge2-0_2']
-        green_lanes = ['edge3-0_0', 'edge3-0_1', 'edge3-0_2', 'edge4-0_0', 'edge4-0_1', 'edge4-0_2']
-    else:
-        red_lanes = ['edge3-0_0', 'edge3-0_1', 'edge3-0_2', 'edge4-0_0', 'edge4-0_1', 'edge4-0_2']
-        green_lanes = ['edge1-0_0', 'edge1-0_1', 'edge1-0_2', 'edge2-0_0', 'edge2-0_1', 'edge2-0_2']
-    for lane in red_lanes:
-        vehicle_red.append(traci.lane.getLastStepVehicleNumber(lane))
-    for lane in green_lanes:
-        vehicle_ids = traci.lane.getLastStepVehicleIDs(lane)
-        omega = 0
-        for vehicle_id in vehicle_ids:
-            traci.vehicle.subscribe(vehicle_id, (tc.VAR_DISTANCE, tc.VAR_LANEPOSITION))
-            distance = traci.vehicle.getSubscriptionResults(vehicle_id).get(132)
-            if distance > 100:
-                omega += 1
-        vehicle_green.append(omega)
-
-    return max(vehicle_red), max(vehicle_green)
-
-def get_status_img(current_phase,tl_node_id=node_light_7,area_length=600):
-    mapOfCars = getMapOfVehicles(area_length=area_length)
-
-    current_observation = [mapOfCars]
-    return current_observation
-
 def set_yellow(dic_vehicles,rewards_info_dict,f_log_rewards,rewards_detail_dict_list,node_id="node0"):
     Yellow = "yyyyyyyyyyyyyyyy"
     for i in range(3):
@@ -554,14 +466,6 @@ def set_yellow(dic_vehicles,rewards_info_dict,f_log_rewards,rewards_detail_dict_
         log_rewards(dic_vehicles, 0, rewards_info_dict, f_log_rewards, timestamp, rewards_detail_dict_list)
         update_vehicles_state(dic_vehicles)
 
-def set_all_red(dic_vehicles,rewards_info_dict,f_log_rewards,rewards_detail_dict_list,node_id="node0"):
-    Red = "rrrrrrrrrrrrrrrr"
-    for i in range(3):
-        timestamp = traci.simulation.getTime()/1000
-        traci.trafficlight.setRedYellowGreenState(node_id, Red)
-        traci.simulationStep()
-        log_rewards(dic_vehicles, 0, rewards_info_dict, f_log_rewards, timestamp,rewards_detail_dict_list)
-        update_vehicles_state(dic_vehicles)
 
 def run(action, current_phase, current_phase_duration, vehicle_dict, rewards_info_dict, f_log_rewards, rewards_detail_dict_list,node_id="node0"):
     return_phase = current_phase
@@ -577,14 +481,6 @@ def run(action, current_phase, current_phase_duration, vehicle_dict, rewards_inf
     vehicle_dict = update_vehicles_state(vehicle_dict)
     return return_phase, return_phase_duration+1, vehicle_dict
 
-
-
-def get_base_min_time(traffic_volumes,min_phase_time):
-    traffic_volumes=np.array([36,72,0])
-    min_phase_times=np.array([10,35,35])
-    for i, min_phase_time in enumerate(min_phase_times):
-        ratio=min_phase_time/traffic_volumes[i]
-        traffic_volumes_ratio=traffic_volumes/ratio
 
 def phase_vector_to_number(phase_vector,phases_light=phases_light_7):
     phase_vector_7 = []
